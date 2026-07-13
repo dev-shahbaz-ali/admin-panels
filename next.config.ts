7ada37bf703607187240ecb96ec4060c4e4e2f24
dev-shahbaz-ali <devshahbazali.it@gmail.com>
2026-07-13 16:04:03 +0500

    feat: add TypeScript configuration to allow production builds with type errors

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // This is often used in CI/CD environments where type checking is done as a separate step.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
